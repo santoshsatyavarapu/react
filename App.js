0960644f671acd2a2705414a27755b89e9e7d25a
santoshsatyavarapu <santoshsatyavarapu123@gmail.com>
2024-06-18 22:22:16 +0530

    eposide-02

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
